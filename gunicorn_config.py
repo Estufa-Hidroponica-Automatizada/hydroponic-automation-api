7ac5f59e4200b3d561544ccf101ce1b38c3e22a6
Brian Nunes <brian.nunes@usp.br>
2023-10-06 21:50:42 +0100

    Config rasp

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,5 +2,5 @@
 bind = '0.0.0.0:4000'
 workers = 1
 
-keyfile = '/home/rasp/Documents/hydroponic-automation-api/server.key'
-certfile = '/home/rasp/Documents/hydroponic-automation-api/server.crt' 
+keyfile = '/home/rasp/hydroponic-automation-api/server.key'
+certfile = '/home/rasp/hydroponic-automation-api/server.crt' 
