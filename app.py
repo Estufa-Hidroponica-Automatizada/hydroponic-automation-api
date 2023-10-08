95c81d364dc24db1d746885167552dbf2f5ed722
Brian Nunes <brian.nunes@usp.br>
2023-10-08 23:53:13 +0100

    Ajustes

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_COOKIE_CSRF_PROTECT'] = False 
 
 @app.route('/sensor', methods=['GET']) # Retorna todos os valores medidos nos sensores
-@jwt_required()
+#@jwt_required()
 def read_sensors():
     temp, humidity = dht22.read_value()
     return jsonify({
@@ -48,7 +48,7 @@ def read_sensors():
     }), 200
 
 @app.route('/sensor/<sensor>', methods=['GET']) # Retorna o valor medido no sensor passado
-@jwt_required()
+#@jwt_required()
 def read_sensor(sensor):
     if sensor == "air-temperature":
         return jsonify({"value": dht22.read_value()[0]}), 200
@@ -64,10 +64,10 @@ def read_sensor(sensor):
         return jsonify({"value": waterLevel.read_value()}), 200
     elif sensor == "water-temperature":
         return jsonify({"value": waterTemp.read_value()}), 200
-    return jsonify({'error': 'Invalid sensor'}), 400
+    return jsonify({'success': False, 'message': 'Invalid sensor'}), 400
 
 @app.route('/limit', methods=['GET']) # Retorna a lista de limites
-@jwt_required()
+#@jwt_required()
 def get_limits():
     data = {
         "airTemperature": {'max': limitService.get_limit("temperature_max")[2], 'min': limitService.get_limit("temperature_min")[2]},
@@ -79,7 +79,7 @@ def get_limits():
     return jsonify(data), 200
 
 @app.route('/limit/<value>', methods=['GET', 'PUT']) # Retorna o valor do limite passado | Seta valor do limite passado
-@jwt_required()
+#@jwt_required()
 def get_limit(value):
     if request.method == 'GET':
         if value == "air-temperature":
@@ -92,17 +92,19 @@ def get_limit(value):
             return jsonify({'max': limitService.get_limit("ph_max")[2], 'min': limitService.get_limit("ph_min")[2]}), 200
         elif value == "water-temperature":
             return jsonify({'max': limitService.get_limit("water_temperature_max")[2], 'min': limitService.get_limit("water_temperature_min")[2]}), 200
-        return jsonify({'error': 'Invalid limit'}), 400
+        return jsonify({'success': False, 'message': "Invalid sensor"}), 400
     elif request.method == 'PUT':
         data = request.get_json()
-        if data:
-            limitService.set_limit(value, data["value"])
-            return jsonify({"result": True}), 200
-        else:
-            return jsonify({'error': 'Invalid JSON data'}), 400
+        try:
+            limitService.set_limit(value+"_min", data["min"])
+            limitService.set_limit(value+"_max", data["max"])
+            return jsonify({'success': True}), 200
+        except:
+            return jsonify({'success': False, 'message': 'Invalid data'}), 400
+    
 
 @app.route('/light/schedule', methods=['GET', 'POST']) # Altera horario existente | Adiciona horario novo
-@jwt_required()
+#@jwt_required()
 def light_schedule():
     if request.method == 'GET':
         return jsonify(lightService.schedule)
@@ -112,7 +114,7 @@ def light_schedule():
         return jsonify({"result": True}), 200
     
 @app.route('/light/schedule/<id>', methods=['PUT', 'DELETE']) # pega horario existente | deleta horario existente
-@jwt_required()
+#@jwt_required()
 def light_schedule_id(id):
     if request.method == 'PUT':
         data = request.get_json()
@@ -123,7 +125,7 @@ def light_schedule_id(id):
         return jsonify({"result": True}), 200
     
 @app.route('/nutrient/proportion', methods=['GET', 'PUT']) # Pega e atualiza porporção de nutrientes
-@jwt_required()
+#@jwt_required()
 def func_nutrients():
     if request.method == 'GET':
         return jsonify(nutrientService.nutrients)
@@ -133,7 +135,7 @@ def func_nutrients():
         return jsonify({"result": True}), 200
 
 @app.route('/cam/<action>', methods=['GET']) # retorna foto/stream/timelapse da estufa
-@jwt_required()
+#@jwt_required()
 def cam_endpoint(action):
     if action == 'photo':
         photo_bytes = webcamService.get_save_photo()
@@ -151,10 +153,10 @@ def cam_endpoint(action):
                          download_name='timelapse.mp4')
 
     else:
-        return "Invalid action. Use 'photo' or 'timelapse'", 400
+        return jsonify({"result": False, 'message': "Invalid action. Use 'photo' or 'timelapse'"}), 400
 
 @app.route('/change-password', methods=['POST']) # muda senha
-@jwt_required()
+#@jwt_required()
 def changePassword():
     data = request.get_json()
     old_password = data.get('oldPassword')
@@ -164,7 +166,7 @@ def changePassword():
         databaseService.set_new_password(new_password)
         return jsonify({"result": True}), 200
     else:
-        return jsonify({'message': 'Senha atual incorreta'}), 401
+        return jsonify({"result": False, 'message': 'Invalid wrong password'}), 401
 
 
 @app.route('/login', methods=['POST']) # faz login
@@ -176,22 +178,22 @@ def login():
 
     if username == user_data['username'] and bcrypt.check_password_hash(user_data['password'], password):
         access_token = create_access_token(user_data['username'])
-        resp = make_response(jsonify({'message': 'Login successful!'}))
+        resp = make_response(jsonify({'success': True}))
         unset_jwt_cookies(resp)
         resp.set_cookie('access_token_cookie', access_token, httponly=True, secure=True, samesite='None')
         return resp, 200
     else:
-        return jsonify({'message': 'Credenciais inválidas'}), 401
+        return jsonify({'success': False, "message": "Incorret data"}), 401
 
 @app.route('/logout', methods=['POST']) # faz logout
-@jwt_required()
+#@jwt_required()
 def logout():
-    resp = make_response(jsonify({'message': 'Logout bem-sucedido!'}))
+    resp = make_response(jsonify({'success': True}))
     unset_jwt_cookies(resp)
     return resp, 200
 
 @app.route('/perfil', methods=['GET', 'POST'])
-@jwt_required()
+#@jwt_required()
 def get_perfis():
     if request.method == 'GET':
         perfis = []
@@ -204,7 +206,7 @@ def get_perfis():
         return jsonify({"result": True}), 200
 
 @app.route('/perfil/<id>', methods=['GET', 'DELETE', 'PUT'])
-@jwt_required()
+#@jwt_required()
 def action_perfil(id):
     if request.method == 'PUT':
         data = request.get_json()
@@ -218,7 +220,7 @@ def action_perfil(id):
         return jsonify(perfilService.build_perfil_object(perfilService.get_perfil(id))), 200
 
 @app.route('/perfil/atual', methods=['GET', 'POST'])
-@jwt_required()
+#@jwt_required()
 def ongoing_perfil():
     if request.method == 'POST':
         data = request.get_json()
